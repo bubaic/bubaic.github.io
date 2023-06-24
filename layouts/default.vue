30afd85011db7094dfa4201983754386abff5327
Subrata <bubai@outlook.in>
2023-06-24 22:35:07 +0530

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: #38444d;
+  height: inherit;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Nuxt Routing set up successfully!</h1>
+    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">
+      Learn more about Nuxt Routing</a
+    >
+  </div>
+</template>
